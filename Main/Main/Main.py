1de39eaff31524b4435b785372335eefd7786fec
omekeki <Cookiecat9898>
2017-01-17 14:24:41 +0100

    Update 0.0.2

--- Main/Main/Main.py
+++ Main/Main/Main.py
@@ -1,25 +1,53 @@
-import pygame
 import math
+import pygame
+
 
 class Game:
     def __init__(self):
-        width = 650
-        height = 400
-        demension = (width, height)
+        width = 640
+        height = 480
+        size = (width, height)
 
+        # Start PyGame
         pygame.init()
-
-        self.screen = pygame.display.set_mode(demension)
+        
+        # Set the resolution
+        self.screen = pygame.display.set_mode(size)
+        
+        # Set up the default font
         self.font = pygame.font.Font(None, 30)
 
-        while not event_handler():
-            self.screen.fill((0,0,0))
-            pygame.display.flip()
 
-def event_handler():
+
+    # Draw everything
+    def draw(self):
+        # Clear the screen
+        self.screen.fill((0, 0, 0))
+        
+        # Flip the screen
+        pygame.display.flip()
+        
+    # The game loop
+    def game_loop(self):
+        while not process_events():
+
+            self.draw()
+
+# Handle pygame events
+def process_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            # Give the signal to quit
             return True
+    
     return False
 
-Game()
+
+# Main program logic
+def program():
+    game = Game()
+    game.game_loop()
+
+
+# Start the program
+program()
