d663b9204e4f0608565f606ef81ddf23248d80bb
omekeki <Cookiecat9898>
2017-01-17 14:11:34 +0100

    Update 0.0.1

--- Main/Main/Main.py
+++ Main/Main/Main.py
@@ -1,3 +1,25 @@
-class Main:
+import pygame
+import math
+
+class Game:
     def __init__(self):
-        self.name = "Battletoads"
+        width = 650
+        height = 400
+        demension = (width, height)
+
+        pygame.init()
+
+        self.screen = pygame.display.set_mode(demension)
+        self.font = pygame.font.Font(None, 30)
+
+        while not event_handler():
+            self.screen.fill((0,0,0))
+            pygame.display.flip()
+
+def event_handler():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return True
+    return False
+
+Game()
