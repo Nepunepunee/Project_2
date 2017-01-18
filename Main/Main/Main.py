88fc595e71634f2a99c72a336ae64cb552c901dd
CasperCX <0927578@hr.nl>
2017-01-18 12:02:04 +0100

    0.0.7
    Added colors and start for image-loading for start menu, still doesnt
    work

--- Main/Main/Main.py
+++ Main/Main/Main.py
@@ -1,15 +1,23 @@
 import math
 import pygame
-#set FPS
-FPS = 30
-clock = pygame.time.Clock()
 
+##colors
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+black = (0,0,0)
+white = (255,255,255)
+
+
+def load_image(filename: str) -> pygame.Surface:
+    surface = pygame.image.load(filename).convert()
+    return surface
 
 class Game:
     def __init__(self):
-        width = 640
-        height = 480
-        size = (width, height)
+        self.width = 640
+        self.height = 480
+        size = (self.width, self.height)
 
         # Start PyGame
         pygame.init()
@@ -24,23 +32,34 @@ class Game:
         self.font = pygame.font.Font(None, 30)
 
         #Set up FPS
+        FPS = 30
+        clock = pygame.time.Clock()
         clock.tick(FPS)
 
 
-
     # Draw everything
-    def draw(self):
+    def draw_menu(self):
         # Clear the screen
-        self.screen.fill((0, 0, 0))
-        
+        self.screen.fill((white))
+        # draw block for menu items
+
+        # load main menu image
+        # load_image('main_menu.png')
+
         # Flip the screen
         pygame.display.flip()
-        
+
+        #message drawer
+    def message_to_screen(self,msg, color):
+        screen_text = self.font.render(msg, True, color)
+        self.screen.blit(screen_text, [self.width / 2, self.height / 2])
+
     # The game loop
     def game_loop(self):
         while not process_events():
 
-            self.draw()
+            self.draw_menu()
+
 
 # Handle pygame events
 def process_events():
