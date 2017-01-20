a37d17d00f08550e18ca6efcdcece489ac09e042
CasperCX <0927578@hr.nl>
2017-01-20 11:58:17 +0100

    updated Game.py

--- Main/Game.py
+++ Main/Game.py
@@ -1,3 +1,4 @@
+
 import math
 import pygame,sys,random,os
 from pygame.locals import *
@@ -16,8 +17,8 @@ textures = {
 
 #useful game dimensions
 TILESIZE = 40
-MAPWIDTH = 20
-MAPHEIGHT = 20
+MAPWIDTH = 15
+MAPHEIGHT = 15
 
 #a list of recources
 recources = [WATER]
@@ -37,12 +38,13 @@ DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE))
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
+lightblue = (0,200,200)
 black = (0, 0, 0)
 white = (255, 255, 255)
 
 
-width = 640
-height = 480
+width = (MAPWIDTH * 40) +(200)
+height = (MAPHEIGHT * 40) + (200)
 menu = True
 size = (width, height)
 # Start PyGame
@@ -122,16 +124,17 @@ def mainloop():
     # click = pygame.mouse.get_pressed()
 
     while not gameExit:
+        grid = False
+        print (grid)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quitgame()
                 gameExit = True
 
-            # if event.type == pygame.MOUSEBUTTONDOWN:
-            #     mousepos = pygame.mouse.get_pos()
-            #     message_to_screen(str(mousepos),black,30,600)
-            #     if mousepos > player1_posX :
-            #         print ("player 1 detected")
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mousepos = pygame.mouse.get_pos()
+                new_position = []
+
 
             if event.type == KEYDOWN:
                 # if right arrow is pressed
@@ -162,36 +165,38 @@ def mainloop():
                     # change player's y position
                     player2Pos[1] += 1
 
-       ##ERROR
-        # loop through each row
-        for rw in range(MAPHEIGHT):
-            for cl in range(MAPWIDTH):
-                randomnumber = random.randint(0, 15)
-                if randomnumber >= 1 or randomnumber <= 10:
-                    tile = WATER
+        if grid == False:
+            for rw in range(MAPHEIGHT):
+                for cl in range(MAPWIDTH):
+                    randomnumber = random.randint(0, 15)
+                    if randomnumber >= 1 or randomnumber <= 10:
+                        tile = WATER
 
-                tilemap[rw][cl] = tile
+                    tilemap[rw][cl] = tile
+                    screen.fill(lightblue)
 
-            for row in range(MAPHEIGHT):
-                for column in range(MAPWIDTH):
-                    # draw the resource at that position in the tilemap, using the correct image
-                    DISPLAYSURF.blit(textures[tilemap[row][column]], (column * TILESIZE, row * TILESIZE))
-            # display the player at the correct position
-            DISPLAYSURF.blit(player1, (player1Pos[0] * TILESIZE, player1Pos[1] * TILESIZE))
-            DISPLAYSURF.blit(player2, (player2Pos[0] * TILESIZE, player2Pos[1] * TILESIZE))
-
-
-
-            # screen writings
-            mousepos = pygame.mouse.get_pos()
-            message_to_screen(str(mousepos),red,500,15)
-            # message_to_screen("player ships: " + str(player1.ships), red,10,10)
-            # message_to_screen("player ships: " + str(player2.ships), blue, 200, 10)
-            # message_to_screen("HP: " + str(player1.hp), red, 10, 30)
-            # message_to_screen("HP: " + str(player2.hp), blue, 200, 30)
+                for row in range(MAPHEIGHT):
+                    for column in range(MAPWIDTH):
+                        # draw the resource at that position in the tilemap, using the correct image
+                        DISPLAYSURF.blit(textures[tilemap[row][column]], (column * TILESIZE, row * TILESIZE))
+                # display the player at the correct position
+                DISPLAYSURF.blit(player1, (player1Pos[0] * TILESIZE, player1Pos[1] * TILESIZE))
+                DISPLAYSURF.blit(player2, (player2Pos[0] * TILESIZE, player2Pos[1] * TILESIZE))
+                grid = True
 
 
-            pygame.display.update()
+
+                # screen writings
+                mousepos = pygame.mouse.get_pos()
+                message_to_screen("PLAYER1 " + str(player1Pos),red,650,15)
+                message_to_screen("PLAYER2 " + str(player2Pos), blue, 650, 40)
+                # message_to_screen("player ships: " + str(player1.ships), red,10,10)
+                # message_to_screen("player ships: " + str(player2.ships), blue, 200, 10)
+                # message_to_screen("HP: " + str(player1.hp), red, 10, 30)
+                # message_to_screen("HP: " + str(player2.hp), blue, 200, 30)
+
+
+                pygame.display.update()
 
 
 
@@ -206,4 +211,6 @@ game = Game()
 
 
 game()
+
+
 
